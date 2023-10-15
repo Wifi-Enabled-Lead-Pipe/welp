9ad1f9ea14fac5fb8e8e4343dfc54aadead7ca75
cpkoutras <cpkoutras@gmail.com>
2023-10-14 20:21:20 -0400

    feat (game-logic): game logic controller complete

--- SubSystems/GameLogic/GameLogicController.cs
+++ SubSystems/GameLogic/GameLogicController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Welp.GameData;
 
 namespace Welp.GameLogic;
 
@@ -6,9 +7,71 @@ namespace Welp.GameLogic;
 public class GameLogicController : Controller, IGameLogicService
 {
     private readonly IGameLogicService gameLogicService;
+    private readonly IGameDataService gameDataService;
 
-    public GameLogicController(IGameLogicService gameLogicService)
+    public GameLogicController(IGameLogicService gameLogicService, IGameDataService gameDataService)
     {
         this.gameLogicService = gameLogicService;
+        this.gameDataService = gameDataService;
     }
+
+    /// <summary>
+    /// Game State: Updates the current game state.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpPost]
+    public async Task ApplyPlayerAction(ApplyPlayerActionRequest request) =>
+        await gameLogicService.ApplyPlayerAction(request);
+
+    /// <summary>
+    /// Card Management: Handles card distribution.
+    /// </summary>
+    /// <param name="GameId"></param>
+    /// <returns></returns>
+    [HttpPut]
+    public async Task DistributeCards(Guid GameId)
+    {
+        // do something
+    }
+
+    /// <summary>
+    /// Game Rules Engine: Enforces the rules of ClueLess.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost("evaluate-player-action")]
+    public async Task<EvaluatePlayerActionResponse> EvaluatePlayerAction(
+        EvaluatePlayerActionRequest request
+    ) => await gameLogicService.EvaluatePlayerAction(request);
+
+    /// <summary>
+    /// Game Rules Engine: Evaluate a Who Done It Scenario
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost("evaluate-who-done-it")]
+    public async Task<EvaluateWhoDoneItResponse> EvaluateWhoDoneIt(
+        EvaluateWhoDoneItRequest request
+    ) => await gameLogicService.EvaluateWhoDoneIt(request);
+
+    /// <summary>
+    /// Game Rules Engine: Provide Player Action Options
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpGet("player-actions")]
+    public async Task<PlayerActionOptionsResponse> GetPlayerActionOptions(
+        PlayerActionOptionsRequest request
+    ) => await gameLogicService.GetPlayerActionOptions(request);
+
+    /// <summary>
+    /// Card Management: Handle Card Tracking
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost("share-card")]
+    public async Task<ShareCardResponse> ShareCard(ShareCardRequest request) =>
+        await gameLogicService.ShareCard(request);
 }
--- SubSystems/GameLogic/Models/GameLogicModels.cs
+++ SubSystems/GameLogic/Models/GameLogicModels.cs
@@ -1,1 +1,61 @@
 namespace Welp.GameLogic;
+
+public class ShareCardResponse
+{
+    public Card Card { get; set; }
+    public Guid ShareFromUserId { get; set; }
+    public Guid ShareToUserId { get; set; }
+}
+
+public class Card
+{
+    public string CardType { get; set; }
+    public string CardName { get; set; }
+}
+
+public class ShareCardRequest
+{
+    public Card Card { get; set; }
+    public Guid ShareFromUserId { get; set; }
+    public Guid ShareToUserId { get; set; }
+}
+
+public class EvaluateWhoDoneItResponse
+{
+    public IEnumerable<Card> Cards { get; set; } = new List<Card>();
+    public Guid SmartyPants { get; set; }
+}
+
+public class EvaluateWhoDoneItRequest
+{
+    public string Status { get; set; }
+}
+
+public class PlayerActionOptionsRequest
+{
+    public Guid ActorId { get; set; }
+    public string ActionType { get; set; }
+    public object ActionData { get; set; }
+}
+
+public class ApplyPlayerActionRequest
+{
+    public Guid ActorId { get; set; }
+    public string ActionType { get; set; }
+    public object ActionData { get; set; }
+}
+
+public class EvaluatePlayerActionRequest
+{
+    public string Status { get; set; }
+}
+
+public class PlayerActionOptionsResponse
+{
+    public Guid PlayerId { get; set; }
+}
+
+public class EvaluatePlayerActionResponse
+{
+    public string Status { get; set; }
+}
